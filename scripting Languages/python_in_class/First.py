09c7b3149dcce2280c3592683fef6d981dc67b3c
Gideon Tetteh Nyamedi <nyamedigideon1@gmail.com>
2026-05-28 12:49:24 +0000

    SDLC

--- scripting Languages/python_in_class/First.py
+++ scripting Languages/python_in_class/First.py
@@ -5,5 +5,4 @@ def main():
     print((10.5+2 *3)/ 45-3.5)
     print(3+7,3-10)
 
-
 main()
---
+++ scripting Languages/python_in_class/initials.py
@@ -0,0 +1,35 @@
+
+# ************************************************************
+# Name: Gideon Nyamedi
+# Assignment: Initials Program
+# Course: Computer Science
+# Date: May 28, 2026
+# Description:
+# This program prints large stylized initials.
+# ************************************************************
+
+
+
+def main():
+    # Print stylized initials NGT
+
+    print()
+    print("...NGT")
+    print()
+
+    print("...NNNN......NNNN....GGGGGGGGGG....TTTTTTTTTTTT")
+    print("...NNNNN.....NNNN....GGGGGGGGGG....TTTTTTTTTTTT")
+    print("...NNNNNN....NNNN....GGGG............TTTTTT")
+    print("...NNNN.NN...NNNN....GGGG............TTTTTT")
+    print("...NNNN..NN..NNNN....GGGG..GGGGGG....TTTTTT")
+    print("...NNNN...NN.NNNN....GGGG..GGGGGG....TTTTTT")
+    print("...NNNN....NNNNNN....GGGG......GG....TTTTTT")
+    print("...NNNN.....NNNNN....GGGGGGGGGGGG....TTTTTT")
+    print("...NNNN......NNNN....GGGGGGGGGGGG....TTTTTT")
+    print("...NNNN......NNNN....GGGGGGGGGGGG....TTTTTT")
+
+    print()
+
+
+
+main()
