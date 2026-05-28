e9dc44549c87a90ece64a3439c9d6eeed393159f
Gideon Tetteh Nyamedi <nyamedigideon1@gmail.com>
2026-05-28 07:38:12 +0000

    Automator Theory

---
+++ oop/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        // Your code goes here
+        System.out.println("Hello, World!");
+    }
+}
+
+
---
+++ scripting Languages/python_in_class/First.py
@@ -0,0 +1,9 @@
+def main():
+    print("Hello World")
+    print('Hook \'em all!')
+    
+    print((10.5+2 *3)/ 45-3.5)
+    print(3+7,3-10)
+
+
+main()
